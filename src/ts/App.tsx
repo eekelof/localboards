46a18f9958e944de06086e46a0ef1874b981e1b3
eekelof <erik.ekelof1@gmail.com>
2024-09-16 20:48:11 +0200

    fixed deps

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -8,9 +8,6 @@ export default class App {
             <br />
             <br />
             <svg width="24" height="24" viewBox="0 0 24 24"><path d={mdiPlusBoxOutline} />asdf</svg>
-            <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-                <circle r="45" cx="50" cy="50" fill="red" />
-            </svg>
         </div>
 
         const style = {
