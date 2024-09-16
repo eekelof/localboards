62bb6cb59238be2f4dc698bf30f3080730a73d74
eekelof <u@w>
2024-09-16 20:44:02 +0200

    fixed deps

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,13 +1,22 @@
+
+import { mdiPlusBoxOutline } from '@mdi/js';
+
 export default class App {
     static init() {
-        const onClick = (text: string) => alert(text)
         const element = <div>
             <h1>Vite + TypeScript + SoloJSX</h1>
             <br />
-            <div style="cursor: pointer" onclick={() => onClick("Hello")}>Click me</div>
             <br />
-            <img src="/vite.svg" alt="" />
+            <svg width="24" height="24" viewBox="0 0 24 24"><path d={mdiPlusBoxOutline} />asdf</svg>
+            <svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
+                <circle r="45" cx="50" cy="50" fill="red" />
+            </svg>
         </div>
-        document.body.append(element)
+
+        const style = {
+            backgroundColor: "#ff0"
+        }
+        const e = <div style={style}> Hello </div>
+        document.body.append(element, e)
     }
 }
