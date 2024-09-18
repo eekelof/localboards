09b3fd1dcf489c0d79da98a2581e9d87ff8a72bf
eekelof <erik.ekelof1@gmail.com>
2024-09-18 05:00:18 +0200

    fixed cleanup

--- src/ts/elements/board/Board.tsx
+++ src/ts/elements/board/Board.tsx
@@ -1,10 +1,6 @@
-import { List_I } from '../../App';
+import { Board_I } from '../../App';
 import List from './List';
 
-export interface Board_I {
-    id: string;
-    lists: List_I[];
-}
 export default class Board {
     static init(board: Board_I) {
         return <div id="board">
